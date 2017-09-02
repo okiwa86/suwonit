16c33d36b267ab1fd87a8d9837726ad2e313b87b
okiwa86 <okiwa86@gmail.com>
2017-09-02 10:24:17 +0900

    Modify folder tree

--- designPatternStudy/src/dps/adapter/Duck.java
+++
@@ -1,6 +0,0 @@
-package dps.adapter;
-
-public interface Duck {
-	public void quack();
-	public void fly();
-}
--- designPatternStudy/src/dps/iterator/Menu.java
+++
@@ -1,9 +0,0 @@
-package dps.iterator;
-
-import java.util.Iterator;
-
-public interface Menu {
-
-	public Iterator<MenuItem> createIterator();
-
-}
