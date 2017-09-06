af3c68b6983093c9614e38dee61b599dfd2cffd8
Hulk Choi <doksul23@gmail.com>
2017-09-06 20:43:36 +0900

    FactoryMethod book example add

---
+++ 3기/디자인패턴/최혁/workspace/FactoryMethod/src/com/hyuky/Main.java
@@ -0,0 +1,22 @@
+package com.hyuky;
+
+import com.hyuky.framework.Factory;
+import com.hyuky.framework.Product;
+import com.hyuky.idcard.IDCardFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Factory factory = new IDCardFactory(); // Creator IDCard
+		Product card1 = factory.create("Ann");
+		Product card2 = factory.create("Mike");
+		Product card3 = factory.create("Brown");
+		
+		card1.use();
+		card2.use();
+		card3.use();
+		
+		System.out.println(((IDCardFactory) factory).getOwnerList());
+	}
+
+}
---
+++ 3기/디자인패턴/최혁/workspace/FactoryMethod/src/com/hyuky/framework/Product.java
@@ -0,0 +1,5 @@
+package com.hyuky.framework;
+
+public abstract class Product {
+	public abstract void use();
+}
